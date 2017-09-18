690c5545e791e85b1138ae12cdf7ea92eea5e8ed
mseneque <mattseneque@gmail.com>
2017-09-18 23:51:36 +0800

    Refactor code to separate concerns of History search to the SearchService class.

--- Simple-Xamarin-Forms-List/Simple_Xamarin_Forms_List/ListPage.xaml.cs
+++ Simple-Xamarin-Forms-List/Simple_Xamarin_Forms_List/ListPage.xaml.cs
@@ -1,8 +1,7 @@
 ﻿using Simple_Xamarin_Forms_List.Model;
+using Simple_Xamarin_Forms_List.Services;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -11,49 +10,29 @@ namespace Simple_Xamarin_Forms_List
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ListPage : ContentPage
     {
+        private SearchService _searchService;
         private ObservableCollection<History> _history;
 
         public ListPage()
         {
+            _searchService = new SearchService();
+
             InitializeComponent();
 
-            SetMockHistory();
-
-            listView.ItemsSource = _history;
+            _searchService.GetHistory();
         }
 
-
-        // Sets a mock hardcoded history
-        public void SetMockHistory()
-        {
-            _history = new ObservableCollection<History>()
-            {
-                new History {Country = Country.Austraila, StateAbr = StateAbr.WA, Suburb = "Clarkson", CheckIn = DateTime.Today, CheckOut = DateTime.Today.AddDays(2)},
-                new History {Country = Country.Austraila, StateAbr = StateAbr.WA, Suburb = "Dianella", CheckIn = DateTime.Today.AddDays(3), CheckOut = DateTime.Today.AddDays(6)},
-                new History {Country = Country.Austraila, StateAbr = StateAbr.WA, Suburb = "Claremont", CheckIn = DateTime.Today.AddDays(7), CheckOut = DateTime.Today.AddDays(14)}
-            };
-        }
-
-
-        // Implements Search
-        public IEnumerable<History> GetHistory(string searchString = null)
-        {
-            if (string.IsNullOrWhiteSpace(searchString))
-                return _history;
-
-            return _history.Where(c => c.Suburb.ToLower().StartsWith(searchString.ToLower()));
-        }
 
         private void SearchBar_OnTextChanged(object sender, TextChangedEventArgs e)
         {
-            listView.ItemsSource = GetHistory(e.NewTextValue);
+            listView.ItemsSource = _searchService.GetHistory(e.NewTextValue);
         }
 
 
         // Pull to Refresh
         private void ListView_OnRefreshing(object sender, EventArgs e)
         {
-            listView.ItemsSource = GetHistory(searchBar.Text);
+            listView.ItemsSource = _searchService.GetHistory(searchBar.Text);
             //searchBar.Text = "";
             listView.EndRefresh();
         }
---
+++ Simple-Xamarin-Forms-List/Simple_Xamarin_Forms_List/Services/SearchService.cs
@@ -0,0 +1,31 @@
+﻿using Simple_Xamarin_Forms_List.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace Simple_Xamarin_Forms_List.Services
+{
+    public class SearchService
+    {
+        // Sets a mock hardcoded history
+        private ObservableCollection<History> _history = new ObservableCollection<History>
+        {
+            new History {Country = Country.Austraila, StateAbr = StateAbr.WA, Suburb = "Clarkson", CheckIn = DateTime.Today, CheckOut = DateTime.Today.AddDays(2)},
+            new History {Country = Country.Austraila, StateAbr = StateAbr.WA, Suburb = "Dianella", CheckIn = DateTime.Today.AddDays(3), CheckOut = DateTime.Today.AddDays(6)},
+            new History {Country = Country.Austraila, StateAbr = StateAbr.WA, Suburb = "Claremont", CheckIn = DateTime.Today.AddDays(7), CheckOut = DateTime.Today.AddDays(14)}
+        };
+
+
+        // Implements Search
+        public IEnumerable<History> GetHistory(string searchString = null)
+        {
+            if (string.IsNullOrWhiteSpace(searchString))
+                return _history;
+
+            return _history.Where(s => s.Suburb.StartsWith(searchString, StringComparison.CurrentCultureIgnoreCase));
+
+        }
+
+
+    }
